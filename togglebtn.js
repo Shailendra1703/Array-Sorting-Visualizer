4ee7b817037cad2525acd1d731356df01b685260
zenocodes <azronebrian@gmail.com>
2022-10-21 15:36:30 +0300

    included the title styling on change

--- togglebtn.js
+++ togglebtn.js
@@ -11,8 +11,10 @@ checkbox.addEventListener('change', function() {
     if(this.checked) {
         trans()
         document.documentElement.setAttribute('data-theme', 'light')
+        document.querySelector('#title').style.color = '#000'
     } else {
         trans()
         document.documentElement.setAttribute('data-theme', 'dark')
+        document.querySelector('#title').style.color = '#fff'
     }
 })
